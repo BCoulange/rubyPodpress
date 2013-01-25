b5fb58fdba1dcbcd0f226804b2556781cbf4884d
Baptiste Coulange <bc@cornis.fr>
2013-01-25 18:07:56 +0100

    ajout de texte sur les images

--- PodcastScience/116/20130125_bestCoeffs.rb
+++ PodcastScience/116/20130125_bestCoeffs.rb
@@ -1,6 +1,8 @@
 # script qui génère des jolies images compressées selon Fourier
 require 'RMagick'
 
+include Magick
+
 def evenize(number)
 	if number%2==0 then
 		return number
@@ -19,11 +21,28 @@ nbCoeffMax=i.columns*i.rows
 
 Dir.mkdir(foldSave) unless File.exists?(foldSave) 
 
-
 nbCoeffs=2
 while nbCoeffs < nbCoeffMax
 	`./keepBestnDFTCoeffs #{inFile} #{nbCoeffs} #{foldSave+evenize(nbCoeffs.to_i).to_s}.jpg`
+
+
+	# Ajout de texte descriptif
+	i = ImageList.new("#{foldSave+evenize(nbCoeffs.to_i).to_s}.jpg")
+	txt = Draw.new
+		i.annotate(txt, 0,0,0,0,"#{evenize(nbCoeffs.to_i).to_s} rayures"){
+		txt.gravity = Magick::SouthGravity
+		txt.pointsize = 25
+		txt.stroke = '#000000'
+		txt.fill = '#ffffff'
+		txt.font_weight = Magick::BoldWeight
+	}
+	i.format = 'jpeg'
+
+	i.write("#{foldSave+evenize(nbCoeffs.to_i).to_s}.jpg")
+
+
 	nbCoeffs*=1.2
 
-
 end
+
+
