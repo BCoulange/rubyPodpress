65d5b1345f77d3fa1abb5fb1ae23b09925b9bdba
Baptiste Coulange <bc@cornis.fr>
2013-01-25 09:02:24 +0100

    selection des meilleurs pixels de la fft ok

--- PodcastScience/116/opencvDFT.cpp
+++ PodcastScience/116/opencvDFT.cpp
@@ -3,12 +3,17 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 
+using namespace std;
 using namespace cv;
 
 int main(int argc, char ** argv)
 {
     const char* filename = argc >=2 ? argv[1] : "lena.jpg";
 
+
+    int nbCoeff2Keep = argc >= 3 ? atoi(argv[2]) : -1 ;
+
+    // Chargement de l'image
     Mat I = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
     if( I.empty())
         return -1;
@@ -22,6 +27,7 @@ int main(int argc, char ** argv)
     Mat complexI;
     merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
 
+    // Calcul de la fft
     dft(complexI, complexI);            // this way the result may fit in the source matrix
 
     // compute the magnitude and switch to logarithmic scale
@@ -33,10 +39,45 @@ int main(int argc, char ** argv)
     magI += Scalar::all(1);                    // switch to logarithmic scale
     log(magI, magI);
 
+
+
+
+
+
+// Création et initialisation du vecteur
+std::vector <float> ivec;
+ 
+
+    // compute sum of positive matrix elements, iterator-based variant
+
+    double sum=0;
+    MatConstIterator_<float> it = magI.begin<float>(), it_end = magI.end<float>();
+    for(; it != it_end; ++it)
+       ivec.push_back (*it);
+
+// Tri du vecteur grâce à la fonction std::sort
+std::sort (ivec.begin(), ivec.end(), std::greater <float>());
+
+//std::sort (magI.begin<float>(), magI.end<float>(), std::greater <float>());
+
+//for (vector<float>::iterator it2 = ivec.begin(); it2!=ivec.begin()+nbCoeff2Keep; ++it2) 
+//    cout << *it2 << endl;
+
+cout << ivec[nbCoeff2Keep] << endl;
+
+// acceder aux valeurs des pixels et les changer!!!
+for(int i=1;i<magI.rows;i++)
+    for(int j=1;j<magI.cols;j++){
+        magI.at<float>(i,j) = magI.at<float>(i,j)>ivec[nbCoeff2Keep]?magI.at<float>(i,j):0.;
+    }
+
+
     // crop the spectrum, if it has an odd number of rows or columns
+
     magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
 
     // rearrange the quadrants of Fourier image  so that the origin is at the image center
+
     int cx = magI.cols/2;
     int cy = magI.rows/2;
 
@@ -56,7 +97,7 @@ int main(int argc, char ** argv)
 
     normalize(magI, magI, 0, 255, CV_MINMAX); // Transform the matrix with float values into a
                                             // viewable image form (float between values 0 and 1).
- 
+
 
     imwrite( "Gray_Image.jpg", magI );
 
